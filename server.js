189eea47b64649cf4ec0db2332eb6ed1b9533ccf
Ryan St.Pierre <ras70@duke.edu>
2016-12-23 13:04:40 -0500

    Trying to get basic hello world server

--- server.js
+++ server.js
@@ -2,10 +2,11 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-app.use(express.static('./build'));
+//app.use(express.static('./build'));
 
 app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, './build', 'index.html'));
+  res.send("Hello")
+  //res.sendFile(path.join(__dirname, './build', 'index.html'));
 });
 
-app.listen(9000);
+app.listen(8080);
