bb1ad344c2f8af86f7150c7535adc810924817da
Ryan St.Pierre <ras70@duke.edu>
2016-12-26 15:20:37 -0500

    Logging

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 var express = require('express');
 var app = express();
+const port = 3000;
 
 app.get('/', function(req, res){
   res.send('hello world');
 });
 
-app.listen(3000);
+app.listen(port);
+console.log("listening on " + port)
