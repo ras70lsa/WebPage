013b27730304bbc13d53cc7ed0d70006bde2e19b
Ryan St.Pierre <ras70@duke.edu>
2016-12-26 16:28:37 -0500

    pushed again, hello worked trying to push html

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var app = express();
+const path = require('path');
 const port = 3000;
 
+app.use(express.static('./build'));
+
 app.get('/', function(req, res){
-  res.send('hello');
+  res.sendFile(path.join(__dirname, './build', 'index.html'));
 });
 
 app.listen(port);
